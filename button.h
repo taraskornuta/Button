eb65ba69113676b1565cab288c22f3c4aa54d9c6
Taras Kornuta <taraskornuta@gmail.com>
2020-12-12 00:07:39 +0200

    Update button.h
    fix own name1

--- button.h
+++ button.h
@@ -4,7 +4,7 @@
 /**
   * MIT License
   * 
-  * Copyright (c) 2020 Tatas Kornuta (Leon11t)
+  * Copyright (c) 2020 Taras Kornuta (Leon11t)
   * 
   * Permission is hereby granted, free of charge, to any person obtaining a copy
   * of this software and associated documentation files (the "Software"), to deal
